8e90584ece5cab23c1699ffb11b17bdbcd1f3f40
Carlos Alvarez <calvarez.cl011@gmail.com>
2026-07-12 15:20:18 -0600

    Integracion de los middleware

--- index.js
+++ index.js
@@ -5,9 +5,32 @@ const app = express()
 // declara e inicializa las variables de entorno 
 dotenv.config() 
 
-// Intercepta las peticiones y las respuestas 
+/*
+## 4. Middlewares Requeridos
+
+1.  **Middleware de Logging (Global):**
+    Un middleware que imprima en la consola de la terminal cada petición entrante con el formato:
+    `[FECHA/HORA] METODO - /ruta - Body: {...}`
+
+3.  **Middleware de Manejo de Errores (Global):**
+    Capturar cualquier error no controlado en la aplicación y devolver un JSON genérico con código `500 Internal Server Error`.
+*/
 app.use(express.json()) 
 
+const loggerMiddleware = (req, res, next) => {
+  const fechaHora = new Date().toISOString();
+  console.log(`[${fechaHora}] ${req.method} - ${req.originalUrl} - Body:`, req.body);
+  next(); 
+}
+
+app.use(loggerMiddleware)
+
+const errorHandler = (err, req, res, next) => {
+  console.error("Error no controlado:", err);
+  res.status(500).json({ status: 500, message: "Internal Server Error", data: null }); // [6, 7]
+};
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3000
 
---
+++ src/middlewares/validacion.middleware.js
@@ -0,0 +1,24 @@
+/*
+## 4. Middlewares Requeridos
+2.  **Middleware de Validación (Específico):**
+    Una función de orden superior (Higher-Order Function) que reciba un esquema de Zod por parámetro e intercepte la petición. 
+    Si los datos son inválidos, debe retornar un código `400 Bad Request` con el detalle de los errores generados por Zod, 
+    deteniendo la ejecución antes de llegar al controlador.
+*/
+
+
+export const validateSchema = (schema) => (req, res, next) => {
+  // Procesamos la info con safeParse para validar de forma segura [5]
+  const result = schema.safeParse(req.body); 
+  
+  if (!result.success) {
+    return res.status(400).json({
+      status: 400,
+      message: "Errores de validación",
+      data: result.error.errors
+    });
+  }
+  // Si todo es válido, reemplazamos el body con la data parseada (para aplicar el default de tasaInteres)
+  req.body = result.data; 
+  next();
+};
